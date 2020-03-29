bd20be386059a8ca6833293870519ebe519cefb6
Analia Mok <amok@geekhive.com>
2020-03-29 15:53:18 -0400

    Stubbed tests

--- app/Actions/InsertDrupalHelpers.php
+++ app/Actions/InsertDrupalHelpers.php
@@ -3,19 +3,23 @@
 namespace App\Actions;
 
 use App\InteractsWithLando;
+use App\LogsToConsole;
 use App\Stub;
 
 class InsertDrupalHelpers
 {
-    use InteractsWithLando;
+    use LogsToConsole, InteractsWithLando;
 
     public function __invoke(array &$landoFile)
     {
+        $this->info('Tweaking in Drupal Helpers...');
+
         $landoFile['tooling']['phpcs'] = Stub::getYaml('drupal-phpcs');
         $landoFile['tooling']['phpcbf'] = Stub::getYaml('drupal-phpcbf');
         $landoFile['tooling']['drupal-update'] = Stub::getYaml('drupal-update');
         $landoFile['tooling']['catchup'] = Stub::getYaml('catchup');
 
         $this->writeToLandoFile($landoFile);
+        $this->info("SUCCESS! .lando.yml now has Drupal helpers\n");
     }
 }
